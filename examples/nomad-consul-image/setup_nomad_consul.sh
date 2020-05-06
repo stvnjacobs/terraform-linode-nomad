99bf0bf9325858dc735099bc697d42dd692ff697
Steven Jacobs <stjacobs@fastmail.fm>
2020-05-05 23:33:25 -0400

    fix git url

--- examples/nomad-consul-image/setup_nomad_consul.sh
+++ examples/nomad-consul-image/setup_nomad_consul.sh
@@ -5,5 +5,5 @@ set -e
 /tmp/terraform-linode-nomad/modules/install-nomad/install-nomad --version "${NOMAD_VERSION}"
 
 
-git clone --branch "${CONSUL_MODULE_VERSION}"  https://github.com/stvnjacobs/terraform-linode-consul.git /tmp/terraform-linode-consul
+git clone --branch "${CONSUL_MODULE_VERSION}"  https://git.stj.io/stjacobs/terraform-linode-consul.git /tmp/terraform-linode-consul
 /tmp/terraform-linode-consul/modules/install-consul/install-consul --version "${CONSUL_VERSION}"
